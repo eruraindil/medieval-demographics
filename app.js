f63a68347b6bbf08e36bce43e0ddb21bb3448f4e
Matthew Roberts <matt@stikmen.ca>
2015-07-05 22:38:10 -0600

    choose pop density

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-angular.module('myApp', []).controller('appController', function($scope, $http) {
+var app = angular.module('myApp', []);
+
+app.controller('appController', function($scope, $http) {
   $http.get('values.json').then(function(res){
     $scope.values = res.data;
   });
@@ -7,4 +9,36 @@ angular.module('myApp', []).controller('appController', function($scope, $http) 
     //from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  $scope.calcDensity = function(multiplier) {
+    density = 0;
+    for(i = 0; i < 6; i++) {
+      density = density + this.randNum(1,4);
+    }
+    Math.floor(density = density * multiplier / 2);
+
+    if( density < 10 ) {
+      density = 10;
+    } else if (density > 45) {
+      density = 45;
+    }
+    return density;
+  }
 });
+
+app.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    var i = 0;
+    angular.forEach(items, function(item, key) {
+      filtered.push(item);
+      filtered[i]['key'] = key;
+      i++;
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});;
