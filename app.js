81cd5562b112ea7c2e1353b426ae7164acdf8948
Matthew Roberts <matt@stikmen.ca>
2015-07-08 22:14:17 -0600

    playing with population, currently broken

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.controller('appController', function($scope, $http) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  $scope.density = 0;
+  $scope.cities = Array();
+
   $scope.calcDensity = function(multiplier) {
     density = 0;
     for(i = 0; i < 6; i++) {
@@ -22,7 +25,42 @@ app.controller('appController', function($scope, $http) {
     } else if (density > 45) {
       density = 45;
     }
+    $scope.density = density;
     return density;
+  }
+
+  $scope.calcPop = function(i) {
+    pop = i * $scope.density;
+    $scope.cities = this.calcCities(pop);
+    return pop;
+  }
+
+  $scope.calcCountrySize = function(i) {
+    country = i * $scope.density;
+    $scope.cities = this.calcCities(i);
+
+    return country;
+  }
+
+  $scope.calcCities = function(pop) {
+    p = Math.floor(Math.sqrt(pop));
+    m = this.randNum(1,4) + this.randNum(1,4) + 10;
+
+    remPop = pop;
+    while( remPop > 8000) {
+      largestCity = p * m;
+      remPop -= largestCity;
+      sLargestCity = largestCity * (this.randNum(1,4) + this.randNum(1,4)) * .1;
+      remPop -= sLargestCity;
+
+      cities = new Array();
+      while(remPop > 8000) {
+        cities.push(sLargestCity * (this.randNum(1,4) + this.randNum(1,4)) * .05);
+        remPop -= cities[i];
+      }
+
+      return Array(largestCity, sLargestCity).concat(cities);
+    }
   }
 });
 
